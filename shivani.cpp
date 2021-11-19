bd5c0bda93426e7ec7466e45da80f2b50f554d73
shivani <93367797+srishivanic@users.noreply.github.com>
2021-11-19 12:36:53 +0530

    Add files via upload

---
+++ shivani.cpp
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("the number x:");
+    scanf("%d,&x");
+    printf("the number y:");
+    scanf("%d,&y");
+    z=x+y;
+    printf("te number z: %d,z");
+    return(0);
+}
